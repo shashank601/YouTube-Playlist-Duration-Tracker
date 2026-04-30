7151c2c366aa8d8f5f2216bdb24b96f5061aa98b
Shashank <shashank3fr@gmail.com>
2026-05-01 02:02:04 +0530

    add import 'dotenv/config' to the top of db.js, for some reason loaded env var are not reaching pool and resulting in db connection error (i tried changing order of dotenc.config to preceed import pool in server.js but it didnt worked the issue is deeper)

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,27 @@
 import pkg from 'pg';
+import 'dotenv/config'
+// const { Pool } = pkg;
+
+// const pool = new Pool({   // connection setuo no explicit .connect()
+//     user: process.env.PGUSER,
+//     host: process.env.PGHOST,
+//     database: process.env.PGDATABASE,
+//     password: process.env.PGPASSWORD,
+//     port: process.env.PGPORT ? Number(process.env.PGPORT) : 5432,
+// });
+
+
 const { Pool } = pkg;
 
-const pool = new Pool({   // connection setuo no explicit .connect()
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT ? Number(process.env.PGPORT) : 5432,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
+pool.on("connect", () => {
+  console.log("DB connected");
+});
+
+
 
 export default pool;
 
