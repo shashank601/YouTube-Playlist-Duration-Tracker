c3ad27a28c15d631231335c5881d2517ab06b940
Shashank <shashank3fr@gmail.com>
2026-05-01 03:05:40 +0530

    add dockerfile and cors, and allow sevrer to listen on all interfaces

---
+++ backend/Dockerfile
@@ -0,0 +1,12 @@
+FROM node:20-alpine
+
+WORKDIR /app
+
+COPY package*.json ./
+RUN npm ci
+
+COPY . .
+
+ENV NODE_ENV=production
+
+CMD ["npm", "start"]
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import allRoutes from "./src/routes/allRoutes.js";
 import dotenv from "dotenv";
 import cors from "cors";
-import pool from './src/config/db.js';
+import pool from "./src/config/db.js";
 
 dotenv.config();
 
@@ -10,22 +10,25 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", allRoutes);
 
-
-
 async function startServer() {
-  try { 
-    await pool.query('SELECT NOW()');  // test is it live
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
+  try {
+    await pool.query("SELECT NOW()"); // test is it live
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log("Server started...");
     });
   } catch (error) {
-    console.error('Server startup failed:', error);
+    console.error("Server startup failed:", error);
     process.exit(1);
   }
 }
